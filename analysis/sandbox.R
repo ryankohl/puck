a3bebc37b1b5aea7b1bd8e24b32d5a25a8e70f29
Ryan Kohl <ryanckohl@gmail.com>
2013-05-05 17:50:29 -0400

    ignoring json files, mapreduce example with constructs, remote ontology

--- analysis/sandbox.R
+++ analysis/sandbox.R
@@ -21,3 +21,40 @@ result <- mapreduce(
                     map= query.for(actions) )
 ans <- from.dfs(result)
 
+the.file <- "../data/2010-2011/file-8.json"
+the.json <- fromJSON(the.file)
+the.game <- game(the.json)
+the.test <- get.query("select ?desc { ?x a :Penalty . ?x :desc ?desc }")
+the.penalties <- sparql.rdf(the.game, the.test)
+
+penaltyDef <- function(pen, match) {
+  conc("prefix nhl: <http://www.nhl.com/> ",
+       "construct { ?e a nhl:", pen, "} ",
+       " { ?e a nhl:Penalty . ?e nhl:desc ?d . FILTER regex(?d, '.*(",
+       match,
+       ").*')}")
+}
+hooking <- penaltyDef("Hooking", "HOOKING")
+the.model <- construct.rdf(the.game, hooking)
+the.query <- get.query("select ?s ?p ?o { ?s a :Hooking . ?s ?p ?o }")
+the.result <- sparql.rdf(the.model, the.query)
+
+the.ontology <- load.rdf("../data/ontology/ontology.ttl", format="TURTLE")
+penaltyMR <- function(k,v) {
+  game.rdf <- game(v)
+  q <- get.query(conc("select ?game ?type ?desc ?pim {",
+                      "?game :play ?p . ?p a :Penalty . ",
+                      "?p a ?type . ",
+                      "?p :desc ?desc . ",
+                      "?type :penaltyMinutes ?pim } "))
+  penalties.rdf <- construct.rdf(game.rdf, hooking)
+  enhanced1.rdf <- combine.rdf(game.rdf, penalties.rdf)
+  enhanced.rdf <- combine.rdf(enhanced1.rdf, the.ontology)
+  x <- sparql.rdf(enhanced.rdf, q)
+  if (length(x) > 0) { x }
+}
+hdfs.data2= to.dfs(get.data(the.range, the.year))
+result2 <- mapreduce(
+                    input= hdfs.data2,
+                    map= penaltyMR )
+ans2 <- from.dfs(result2)
